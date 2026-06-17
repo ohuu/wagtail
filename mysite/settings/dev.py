8da34ed90803ee53bebb457aa4ecb094baa36e1a
ohuu <oliver@paperplane.software>
2026-06-17 02:18:22 +0100

    init commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-yhn89e!3u#qk8@5$l9()(_js&$5m28b0659*b3a^p@y0&3^q=n"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
